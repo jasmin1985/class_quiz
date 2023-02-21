ca7dbe744ab202e1ac07ee5e5e0d98dee50863a4
jasmin1985 <121196141+jasmin1985@users.noreply.github.com>
2023-02-21 15:08:07 -0500

    Update card.py

--- card.py
+++ card.py
@@ -1,6 +1,15 @@
-# Name:
+# Name:Jasmin Atz
 
 class Card:
-    """class representing a simple Neapolitan card. Holds a 
-    value and a suit"""
+   def __init__("Card"):
+      value = :"Card"(1,10)
+      self.assert(value.Card,10)
+
+   def __repr__("Card"):
+      return.
+
+   def __str__("Card"):
+      str(Card(3, "Cups"))
+      return("3 of Cups")
+
     
